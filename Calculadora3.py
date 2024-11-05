851f4e3fbe3590479b4e75cf20e8bac96311573f
MauriliaVarela <mauriliavarela@gmail.com>
2024-11-05 17:01:21 +0000

    Criei uma lista de operadores válidos
    Descrição: Caso o utilizador insira uma opção indisponível o programa vai inserir uma mensagem de erro.

---
+++ Calculadora3.py
@@ -0,0 +1,69 @@
+def calculadora():
+    lista_contas =[]
+    while True:
+        print("-*" * 25)
+        print("                  MENU DE OPÇÕES                     ")
+        print("-*" * 25)
+        print("Soma : + ")
+        print("Subtração : - ")
+        print(" Multiplicção : * ")
+        print("Divisão : / ")
+        print("Saída : s ")
+        print("hist")
+
+        Operador=input("Escolha uma opção")
+
+        #lista de operadores válidos
+        operadores_validos=["+","-","*","/", "s","hist"]
+
+
+        #Erros/Opção inválida
+        if Operador not in operadores_validos:
+            print("ERRO! Escolha apenas as opções disponíveis.")
+            continue  # volta ao início do loop
+
+        #Sair
+        elif Operador == "s":
+            break
+
+        # Exibir histórico
+        elif Operador == "hist":
+            if lista_contas:
+                print("Histórico de operações:\n")
+                print(lista_contas)
+                print(" Ao todo foram feitas ", len(lista_contas) ,"operações")
+            else:
+                print("Ainda não foram realizadas operações.")
+            continue  # Volta ao menu
+
+
+    # função principal
+
+        #Entrada de números
+        num1=float(input("Insira o primeiro numero: "))
+        num2=float(input("Insira o segundo numero: "))
+
+        #Operações
+        if Operador == "+" :
+            result = num1 + num2
+            print(" A soma dos números inseridos é : " + str(result))
+            lista_contas.append( str(num1) + " + " + str(num2) + " = " + str(result))
+
+        elif Operador == "-" :
+            result = num1-num2
+            print(" A subtração dos números inseridos é : " + str(result))
+            lista_contas.append(str(num1) + " - " + str(num2) + " = " + str(result))
+
+        elif Operador == "/":
+            result = num1/num2
+            print(" A divisão dos número inseridos é : " + str(result))
+            lista_contas.append( str(num1) + " / " + str(num2) + " = " + str(result))
+
+        elif Operador == "*":
+            result = num1*num2
+            print(" A Multiplicação dos números inseridos é : " + str(result))
+            lista_contas.append( str(num1) + " * " + str(num2) + " = " + str(result))
+
+
+calculadora()
+
